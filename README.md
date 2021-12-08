c1e37f6086a196ec45dca9d520cdb48802f7ea20
Edison Wang <n3philim@gmail.com>
2021-12-07 22:30:21 -0800

    Solution for day 1 using Go (2 stars)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AdventOfCode2021
-Solutions for Advent of Code 2021 https://adventofcode.com/2021
+Solutions for Advent of Code 2021 https://adventofcode.com/2021. My goal this year is to familiarize
+myself with Go, Ruby, and Rust, so these would be the languages I would use this time. I optimize
+my code for readability and modularity with documentation if possible.
+
+1. go run aoc1.go
+Number of increasing (window size=1): 1446
+Number of increasing (window size=3): 1486
---
+++ aoc1.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+// Simple function for summing values in an []int.
+func sum(nums []int) (result int) {
+	for _, num := range nums {
+		result += num
+	}
+
+	return result
+}
+
+// Groups a list of numbers into sliding measurement windows of size [winsize], then
+// returns the number of windows whose sum is greater than its previous.
+// Time complexity:  O(n) where n = len(scanner)
+// Space complexity: winsize
+func increasingWithWindow(scanner *bufio.Scanner, winsize int) (increasing int) {
+	lastNums := make([]int, 0)
+
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		lastNums = append(lastNums, num)
+
+		if len(lastNums) > winsize {
+			// Opportunity for memoizing here, but choosing not to for readability.
+			prev, next := sum(lastNums[:winsize]), sum(lastNums[1:])
+
+			if next > prev {
+				increasing++
+			}
+
+			lastNums = lastNums[1:]
+		}
+	}
+
+	return increasing
+}
+
+func main() {
+	input, _ := os.Open("var/aoc1_input.txt")
+	defer input.Close()
+
+	oneIncreasing := increasingWithWindow(bufio.NewScanner(input), 1)
+	input.Seek(0, io.SeekStart)
+	threeIncreasing := increasingWithWindow(bufio.NewScanner(input), 3)
+
+	fmt.Printf("Number of increasing (window size=1): %d\n", oneIncreasing)
+	fmt.Printf("Number of increasing (window size=3): %d\n", threeIncreasing)
+}
